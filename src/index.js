55a579517df1421c4c1ec4714c9ebe7bdb8305cd
Ferdian Afza <farisafza201002@gmail.com>
2024-05-23 09:12:54 +0700

    * cors

--- src/index.js
+++ src/index.js
@@ -19,7 +19,12 @@ const provinceModel = require('./models/provincies');
 const app = express();
 const cors = require('cors');
 
-app.use(cors());
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 204
+  };
+  
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(middlewareLogRequest);
